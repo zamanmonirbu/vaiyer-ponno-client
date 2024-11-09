2fbb8a8387838d873231b6b1c84f56de25d5f8e1
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2024-11-10 00:13:34 +0600

    for defence final

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -5,8 +5,8 @@ import store from '../store';
 
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://vaiyer-ponno-backend.onrender.com',
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://vaiyer-ponno-backend.onrender.com',
+  // baseURL: 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json',
   },
